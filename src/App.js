61cfe2b177571255b3d329688d4e0fcca7670a2f
cs10002 <cs10002bd@gmail.com>
2020-02-12 21:46:30 +0900

    Material UI를 적용하여 테이블 만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import DartkingList from './components/DartkingList';
+import DartkingList from './components/DartkingList';   // List 
+import Paper from '@material-ui/core/Paper'             // 페이퍼 
+import Table from '@material-ui/core/Table'             // 테이블 
+import TableHead from '@material-ui/core/TableHead'     // 테이블 헤더
+import TabTableBodyle from '@material-ui/core/TableBody'// 테이블바디
+import TableRow from '@material-ui/core/TableRow'       // 테이블 row
+import TableCell from '@material-ui/core/TableCell'     // 테이블 col  
+import TableBody from '@material-ui/core/TableBody';
+import { withStyles } from '@material-ui/core/styles';  // CSS 스타일 적용
 
-// json 배열로 만들기
+
+// 스타일적용
+const styles = theme =>({
+  root:{
+     width:"100%",
+     margintop:theme.spacing.unit*3,
+     overflowX:"auto"
+  },
+  table:{
+    minWidth:1080
+  }
+});
+
+// json 만들기
 // const member = {
 //   'id': 1,
 //   'image': 'https://placeimg.com/64/64/any',
@@ -29,7 +50,7 @@ const member = [
     'id': 2,
     'image': 'https://placeimg.com/64/64/any',
     'name': '서용태',
-    'nicname': '18용태',
+    'nicname': '수용태',
     'age': '33',
     'gender': '남자',
     'vp': 20
@@ -39,10 +60,20 @@ const member = [
     'id': 3,
     'image': 'https://placeimg.com/64/64/any',
     'name': '오태준',
-    'nicname': '오점',
-    'age': '24',
+    'nicname': '오짱',
+    'age': '34',
     'gender': '남자',
     'vp': 30
+  } 
+  ,
+  {
+    'id': 4,
+    'image': 'https://placeimg.com/64/64/any',
+    'name': '이혜광',
+    'nicname': '혜광불패',
+    'age': '41',
+    'gender': '남자',
+    'vp': 31
   } 
 
 ]
@@ -50,6 +81,7 @@ const member = [
 // React DartKingList Start1
 class App extends Component{
   render(){
+    const{classes } = this.props;
     return (
       // list
          /*<DartkingList
@@ -59,13 +91,28 @@ class App extends Component{
            age={member.age}
            vp={member.vp}                  
          />*/      
-       <div>
-         {member.map( i =>{
-           return <DartkingList key={i.id} id={i.id} image={i.image} age={i.age} vp={i.vp}/>
-         })}         
-       </div>
+       <Paper>
+         <Table className={classes.root}>{/* 테이블 */}
+           <TableHead className={classes.table}> {/*테이블 헤더*/}
+             <TableRow>{/*테이블 Row*/}
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>나이</TableCell>
+              <TableCell>닉네임</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>승점</TableCell>
+             </TableRow>
+           </TableHead>
+           <TableBody>
+            {member.map( i =>{
+                return <DartkingList key={i.id} id={i.id} name={i.name} nicname={i.nicname} image={i.image} age={i.age} vp={i.vp} gender={i.gender}/>
+              })} 
+           </TableBody>
+         </Table>
+       </Paper>
     );
   }
 }
-
-export default App;
+// Styles 적용
+export default withStyles(styles)(App);
--- src/components/DartkingList.js
+++ src/components/DartkingList.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';  //  테이블 Row  만들기
+import TableCell from '@material-ui/core/TableCell' //  테이블 cell 만들기
+
 
 // dartKingList Component 
 class DartkingList extends React.Component{   
    render(){
       const pr  = this.props;
        return(
-        <div>
-           <MemberInfo image={pr.image} name={pr.name} age={pr.age} nicname ={pr.nicname} />
-           <MemberVp   vp={pr.vp} />
-        </div>
-       )
+          <TableRow>
+             <TableCell>{pr.id}</TableCell>                                {/* id */}
+             <TableCell><img src={pr.image} alt = "profile" /></TableCell> {/* image */}
+             <TableCell>{pr.name}</TableCell>   {/* 이름  */}
+             <TableCell>{pr.age}</TableCell>    {/* 나이  */}
+             <TableCell>{pr.nicname}</TableCell>{/* 닉네임*/}
+             <TableCell>{pr.gender}</TableCell> {/* 성별  */}
+             <TableCell>{pr.vp}</TableCell>     {/* 승점  */}
+          </TableRow>
+      )
    }
 }
 
