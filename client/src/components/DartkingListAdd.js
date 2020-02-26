56f67653e11522a140ec0e91ae442cef616cb90c
cs10002 <cs10002bd@gmail.com>
2020-02-26 20:57:43 +0900

    13강 Express에서 파일 업로드 요청 처리 및 DB에 데이터 삽입
    출처: https://ndb796.tistory.com/223?category=1030599 [안경잡이개발자]

--- client/src/components/DartkingListAdd.js
+++ client/src/components/DartkingListAdd.js
@@ -16,7 +16,8 @@ class DartkingListAdd extends React.Component{
             nicName:'',
             brdt:'',
             gender:'',
-            fileName:''
+            fileName:'',
+            appgubn:''
         }
     }
 
@@ -28,15 +29,33 @@ handleFormSubmit=(e)=> {
    .then((response) => {
    console.log(response.data);
    })
+
+   this.setState({
+    file:null,
+    userName:'',
+    id:'',
+    pass:'',
+    name:'',
+    nicName:'',
+    brdt:'',
+    gender:'',
+    fileName:'',
+    appgubn:''
+   })
+   window.location.reload();
 }
     
 
 // 파일변경시
 handleFileChange=(e)=>{
-  this.setState({
-    file:e.target.file[0],
-    fileName:e.target.value
+ 
+ console.log(e.target);
+
+  this.setState({    
+    file: e.target.files[0],
+    fileName: e.target.value    
   });
+
 }
 // value변경
 handleValueChange=(e)=>{
@@ -54,9 +73,10 @@ const formData = new FormData();
  formData.append('id',      this.state.id);
  formData.append('pass',    this.state.pass);
  formData.append('name',    this.state.name);
- formData.append('nicName', this.state.nicName);
+ formData.append('nicname', this.state.nicName);
  formData.append('brdt',    this.state.brdt);
  formData.append('gender',  this.state.gender);
+ 
 
  const config = {
         headers: {        
@@ -75,7 +95,7 @@ render(){
             패스워드:<input type="text" name="pass" value={this.state.pass} onChange={this.handleValueChange}/><br/>
             프로필이미지:<input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
             이름:<input type="text" name="name" value={this.state.name} onChange={this.handleValueChange}/><br/>
-            닉네임:<input type="text" name="nicName" value={this.state.namnicNamee} onChange={this.handleValueChange}/><br/>
+            닉네임:<input type="text" name="nicName" value={this.state.nicName} onChange={this.handleValueChange}/><br/>
             생년월일:<input type="text" name="brdt" value={this.state.brdt} onChange={this.handleValueChange}/><br/>
             성별:<input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
             <button type="submit">추가하기</button>
--- server/server.js
+++ server/server.js
@@ -7,9 +7,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // mySql 연동
-const data  = fs.readFileSync('../database.json');
-const conf  = JSON.parse(data);
-const mysql = require('mysql');
+const data   = fs.readFileSync('../database.json');
+const conf   = JSON.parse(data);
+const mysql  = require('mysql');
+const multer = require('multer');
+const upload = multer({dest: '../upload'})
 
 const connection = mysql.createConnection({
     host: conf.host,
@@ -21,17 +23,43 @@ const connection = mysql.createConnection({
   
 connection.connect();
 
-let query = "SELECT A.seq ,A.appgbn ,A.id ,A.auth ,A.name ,A.image ,A.nicname";
-    query += ",FLOOR( (CAST(REPLACE(CURRENT_DATE,'-','') AS UNSIGNED) - CAST(REPLACE(A.brdt,'.','') AS UNSIGNED)) / 10000 ) age";
-    query += ",IF (A.gender= '1','남자','여자')gender FROM MEMBERS A";
 
     //REST API
 app.get('/api/members',(req,res)=>{    
+  let query = "SELECT A.seq ,A.appgbn ,A.id ,A.auth ,A.name ,A.image ,A.nicname";
+      query += ",FLOOR( (CAST(REPLACE(CURRENT_DATE,'-','') AS UNSIGNED) - CAST(REPLACE(A.brdt,'.','') AS UNSIGNED)) / 10000 ) age";
+      query += ",IF (A.gender= '1','남자','여자')gender FROM MEMBERS A";    
    connection.query(query
      ,(err, rows, fields) => {
           res.send(rows);
     }
   ) 
+});
+
+
+app.use('/image', express.static('../upload'));
+
+app.post('/api/members', upload.single('image'),(req, res) =>{
+   let sql    = 'INSERT INTO MEMBERS (seq , appgbn, id, pass, auth, name, image, nicname,brdt, gender, regDt, updDt) VALUES(null,?,?,?,?,?,?,?,?,?,now(),now())';   
+   let appgbn    = 'a001';
+   let id        = req.body.id;
+   let pass      = req.body.pass;
+   let auth      = 'a2';
+   let name      = req.body.name;
+   let image     =  '/image/' + req.file.filename;
+   let nicname   = req.body.nicname;
+   let brdt      = req.body.brdt;
+   let gender    = req.body.gender;
+
+   console.log(image);
+   let params    =[appgbn,id,pass,auth,name,image,nicname,brdt,gender]
+   
+   console.log(params);
+   connection.query(sql,params,(err,rows,fields)=>{
+     console.log(err);
+      res.send(rows);
+   });
+
 });
 
 
